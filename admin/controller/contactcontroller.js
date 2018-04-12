98549d0c7c873c2f92c269b2cd8e19d9a0655541
nasim <nasimudheen28@gmail.com>
2018-04-12 23:44:04 +0530

    somethingddd\

--- admin/controller/contactcontroller.js
+++ admin/controller/contactcontroller.js
@@ -89,8 +89,8 @@ var mailOptions = {
   to: 'info@stayhappi.in',
   cc: 'anil.sharma@stayhappi.in',
   bcc:'nasimudheen97@gmail.com',
-  subject: 'SarvaGunAushdhi Website Contact Inforamtion ',
-  html: '<html><body style="background-color: #f5f5f5;color: #7b6868;font-size: 15px;margin: 19px;padding: 60px"><p style=" color:#7b6868;">You Have a New Enquiry,</p> From <br><br> Name : '+name+ ',<br/> <br/><br/><br/></b> Email :'+email+'<br/><br/> Phone : '+phone+'</br></br></br></br> Message : '+message+'<br/><br/><br/></body></html>'
+  subject: 'SarvaGunAushdhi Website Partner With Us Inforamtion ',
+  html: '<html><body style="background-color: #f5f5f5;color: #7b6868;font-size: 15px;margin: 19px;padding: 60px"><p style=" color:#7b6868;">You Have a New  Enquiry,</p> <br> <br></b> Email :'+email+'<br/><br/> Phone : '+phone+'</br><br></body></html>'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
