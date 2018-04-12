760b25dcfc696694243a7790999ca4b99741259a
nasim <nasimudheen28@gmail.com>
2018-04-13 02:11:22 +0530

    test

--- admin/controller/contactcontroller.js
+++ admin/controller/contactcontroller.js
@@ -55,7 +55,43 @@ transporter.sendMail(mailOptions, function(error, info){
 
 
 contactCtrl.Dr = function (req, res) {
+     
+
+var email = req.body.email;
+
+var phone = req.body.phone;
+
+
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'noreplaysurvagun@gmail.com',
+    pass: 'survagun123'
+  }
+});
+
+var mailOptions = {
+  from: 'noreplaysurvagun@gmail.com',
+  to: 'info@stayhappi.in',
+  cc: 'anil.sharma@stayhappi.in',
+  bcc:'nasimudheen97@gmail.com',
+  subject: 'SarvaGunAushdhi Website Contact Inforamtion ',
+  html: '<html><body style="background-color: #f5f5f5;color: #7b6868;font-size: 15px;margin: 19px;padding: 60px"><p style=" color:#7b6868;">You Have a New Enquiry,</p> From <br><br>  <br></br> Email :'+email+'<br/><br/> Phone : '+phone+'</br></br></br></body></html>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
           res.json({msg : "successfull"})
+
+  }
+});
+
 
 }
 
--- admin/routes/routMap.js
+++ admin/routes/routMap.js
@@ -36,8 +36,45 @@ module.exports = function(app) {
 
               app.post('/API/contact',multipartMiddleware,contact.apiPOST);
 
-              app.post('/API/mail',contact.Dr);
+              app.post('/API/mail',multipartMiddleware,contact.Dr);
+             app.post('/API/mail', function(req, res) {
 
+              var email = req.body.email;
+
+var phone = req.body.phone;
+
+
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'noreplaysurvagun@gmail.com',
+    pass: 'survagun123'
+  }
+});
+
+var mailOptions = {
+  from: 'noreplaysurvagun@gmail.com',
+  to: 'info@stayhappi.in',
+  cc: 'anil.sharma@stayhappi.in',
+  bcc:'nasimudheen97@gmail.com',
+  subject: 'SarvaGunAushdhi Website Contact Inforamtion ',
+  html: '<html><body style="background-color: #f5f5f5;color: #7b6868;font-size: 15px;margin: 19px;padding: 60px"><p style=" color:#7b6868;">You Have a New Enquiry,</p> From <br><br>  <br></br> Email :'+email+'<br/><br/> Phone : '+phone+'</br></br></br></body></html>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+          res.json({msg : "successfull"})
+
+  }
+});
+    
+                  });
 
               app.post('/API/login',user.login);
 
