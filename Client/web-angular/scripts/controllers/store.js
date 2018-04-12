18ee96924425bc846f8917a271f1093b47c7a280
nasim <nasimudheen28@gmail.com>
2018-04-12 20:44:51 +0530

    jjj

--- Client/web-angular/scripts/controllers/store.js
+++ Client/web-angular/scripts/controllers/store.js
@@ -107,6 +107,72 @@ $scope.save = function() {
 
 
 
+$scope.save1 = function() {
+
+            $scope.submitted = true;
+
+         var payload = new FormData();
+
+         // alert($scope.name);  
+    
+    
+          if ($scope.myForm.$valid) {
+
+     payload.append("name",$scope.name);
+
+
+
+    payload.append("email", $scope.email);
+        payload.append("phone", $scope.phone);
+
+
+
+
+     
+        $http({ 
+            url :BASE_URL+'/test',
+            data :  payload,
+            method : 'POST',
+                headers: {
+                    'Content-type': undefined
+                }
+            })
+            .then(function(response) {
+              // alert();
+     if(response.data.msg=='successfull'){
+       $scope.email = "";
+       $scope.phone = "";
+         $scope.myForm.$setValidity();
+        $scope.myForm.$setPristine();
+        $scope.myForm.$setUntouched(); 
+
+        $scope.successMessage = ' Thank You We Will Get Back to You Soon!';
+     $timeout(function () {
+                                    $scope.successMessage = false;
+       window.location.replace("#/home");
+                                }, 2000);
+        }else{
+
+
+          $scope.successMessage = 'Failed  Added Contact !';
+         $scope.successMessagebool = true;
+            $timeout(function () {
+            $scope.successMessagebool = false;
+           }, 1000);
+
+        }
+
+    
+            });
+          }else{
+   $scope.successMessage = 'Please Enter Columns !';
+    $timeout(function () {
+                                    $scope.successMessage = false;
+       window.location.replace("#/home");
+                                }, 2000);
+
+          }
+        }
 
 
 
