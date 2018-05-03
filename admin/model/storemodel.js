40faabd7ce19a6deb379a5d78a2c0d1c2ee44e0e
nasim <nasimudheen28@gmail.com>
2018-05-03 21:13:05 +0530

    wdwsds

---
+++ admin/model/storemodel.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var StoreSchema = new Schema({
+  Image: {
+    type: String,
+    Required: 'Kindly enter the  the Image'
+  },storename: {
+    type: String,
+    Required: 'Kindly enter Address'
+  },
+  latitude: {
+    type: String,
+    Required: 'Kindly enter latitude'
+  },longttude: {
+    type: String,
+    Required: 'Kindly enter longttude'
+  },
+  email: {
+    type: String,
+    Required: 'Kindly enter the  Email'
+  },
+  phone: {
+    type: String,
+    Required: 'Kindly enter your Description'
+  },
+  location: {
+    type: String,
+    Required: 'Kindly enter location'
+  },
+  address: {
+    type: String,
+    Required: 'Kindly enter Address'
+  },
+   re_address: {
+    type: String,
+    Required: 'Kindly enter Reginel Office Address'
+  }
+});
+module.exports = mongoose.model('Stores', StoreSchema);
+
